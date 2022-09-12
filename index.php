3c4ed6db3d1615acbfa75fde3cfd85b4f18b5f22
ErikBorgogno <erikborgo.20@gmail.com>
2022-09-12 10:00:31 +0200

    add strlen(dimenticanza)

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+    $parola = $_POST['word'];//array associativo
+    $text = 'Ciao a tutti, mi chiamo Erik, ho 25 anni e vivo in provincia di Torino. Sono nato a Carmagnola il 21 gennaio 1997, e tra vari traslochi non ho mai davvero vissuto da qualche parte, non ho mai sentito una città o un posto davvero mio. Tutto è un po\' cambiato da quando vivo da solo...Che bello! Però rimpiango tutti gli anni passati tra una scatola e un altra, perchè non mi è stato permesso di vivere la mia vita al meglio, non mi è stato possibile avere un\'adolescenza, passando dall\'età da bambino ad adulto in un attimo. Insomma, ho perso gli anni migliori della mia vita, e questi non torneranno più indietro';
+    $textCensurato = str_ireplace($parola, '***', $text);
+    $lunghezzaText = strlen($textCensurato);
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,14 +17,9 @@
 </head>
 <body>
 
-    <?php
-        $parola = $_POST['word'];
-        $text = 'Ciao a tutti, mi chiamo Erik, ho 25 anni e vivo in provincia di Torino. Sono nato a Carmagnola il 21 gennaio 1997, e tra vari traslochi non ho mai davvero vissuto da qualche parte, non ho mai sentito una città o un posto davvero mio. Tutto è un po\' cambiato da quando vivo da solo...Che bello! Però rimpiango tutti gli anni passati tra una scatola e un altra, perchè non mi è stato permesso di vivere la mia vita al meglio, non mi è stato possibile avere un\'adolescenza, passando dall\'età da bambino ad adulto in un attimo. Insomma, ho perso gli anni migliori della mia vita, e questi non torneranno più indietro';
-        $text = str_ireplace($parola, '***', $text);
-        
-    ?>
+    <h1><?php echo $textCensurato ?></h1>
+    <h2>Il testo è lungo: <?php echo $lunghezzaText ?> parole.</h2>
 
-    <h1><?php echo $text ?></h1>
 
     
 </body>
